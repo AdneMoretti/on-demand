fd6c80cdd96a808a92050dfda89e3efc94464511
Adne Moretti Moreira <adne.moretti@ifood.com.br>
2025-04-03 09:15:10 -0300

    initialize project

---
+++ src/main/java/adnemoretti/ondemand/on_demand/OnDemandApplication.java
@@ -0,0 +1,13 @@
+package adnemoretti.ondemand.on_demand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnDemandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnDemandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adnemoretti/ondemand/on_demand/OnDemandApplicationTests.java
@@ -0,0 +1,13 @@
+package adnemoretti.ondemand.on_demand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnDemandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
